4c2f5e5a569bfcf8b649e819ae4e24efcef00ead
xiongchenyu6 <xiongchenyu6@gmail.com>
2017-03-14 23:09:04 +0800

    Static files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
+app.use(express.static('public'))
+app.use(express.static('files'))
+app.get('/hey', (req, res) => {
   res.send('HEY!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
